8a202b92d6ce588cf750115d8ccd26ca8590d485
Diksha-dev <68223222+Diksha-dev@users.noreply.github.com>
2020-11-03 13:52:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@
 var express     = require('express');
 var bodyParser  = require('body-parser');
 var errorhandler = require('errorhandler');
+var timeout = require('connect-timeout')
 var http        = require('http');
 var path        = require('path');
 var request     = require('request');
@@ -35,9 +36,27 @@ app.post('/logout', routes.logout );
 // Custom Hello World Activity Routes
 app.post('/journeybuilder/save/', activity.save );
 app.post('/journeybuilder/validate/', activity.validate );
-app.post('/journeybuilder/publish/', activity.publish );
-app.post('/journeybuilder/execute/', activity.execute );
+//app.post('/journeybuilder/publish/', activity.publish );
+app.post('/journeybuilder/publish/', activity.publish,timeout('5s'), bodyParser.json(), haltOnTimedout, function (req, res, next) {
+  savePost(req.body, function (err, id) {
+    if (err) return next(err)
+    if (req.timedout) return
+    res.send('saved as id ' + id)
+  })
+});
+function haltOnTimedout (req, res, next) {
+  if (!req.timedout) next()
+}
 
+function savePost (post, cb) {
+  setTimeout(function () {
+    cb(null, ((Math.random() * 40000) >>> 0))
+  }, (Math.random() * 7000) >>> 0)
+}
+
+
+app.post('/journeybuilder/execute/', activity.execute );
+app.listen(3000)
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
