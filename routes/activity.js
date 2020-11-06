f5c08cb3fe810b76266efce397b576f32dfc70b9
Diksha-dev <68223222+Diksha-dev@users.noreply.github.com>
2020-11-06 18:04:34 +0530

    Update activity.js

--- routes/activity.js
+++ routes/activity.js
@@ -109,7 +109,7 @@ exports.execute = function (req, res) {
           .create({ 
              body: body,
              from: "+12059533166",
-        statusCallback:"https://twilioapp-dk.herokuapp.com/messagestatus",
+        statusCallback:"https://twilioapp-dk.herokuapp.com/journeybuilder/messagestatus",
              to: to
            }) 
          .then(message => console.log(message.sid)) 
