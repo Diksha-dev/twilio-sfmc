1f07a6c7d92d2fc5a3774d869650ac8f2448a81b
Diksha-dev <68223222+Diksha-dev@users.noreply.github.com>
2020-11-10 12:56:16 +0530

    Update activity.js

--- routes/activity.js
+++ routes/activity.js
@@ -111,10 +111,20 @@ exports.execute = function (req, res) {
              from: "+12059533166",
         statusCallback:"https://twilioapp-dk.herokuapp.com/journeybuilder/messagestatus",
              to: to
-           }) 
-         .then(message => console.log(message.status) ) 
-          .done();
- console.log("status"+message.status);
+           },
+                  function(err, responseData) { //this function is executed when a response is received from Twilio
+
+    if (!err) { // "err" is an error received during the request, if any
+
+        // "responseData" is a JavaScript object containing data received from Twilio.
+        // A sample response from sending an SMS message is here (click "JSON" to see how the data appears in JavaScript):
+        // http://www.twilio.com/docs/api/rest/sending-sms#example-1
+
+        console.log(responseData.from); // outputs "+14506667788"
+        console.log(responseData.body); // outputs "word to your mother."
+
+    }
+});
           
 
 
