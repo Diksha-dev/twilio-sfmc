d025a4c7152b2bae2b091467f8f45fc58961f216
Diksha-dev <68223222+Diksha-dev@users.noreply.github.com>
2020-12-01 17:51:35 +0530

    Update activity.js

--- routes/activity.js
+++ routes/activity.js
@@ -132,7 +132,7 @@ exports.execute = function (req, res) {
                   function(err, responseData) { //this function is executed when a response is received from Twilio
 
     if (!err) { // "err" is an error received during the request, if any
-
+console.log("Helooo This is execute");
         // "responseData" is a JavaScript object containing data received from Twilio.
         // A sample response from sending an SMS message is here (click "JSON" to see how the data appears in JavaScript):
         // http://www.twilio.com/docs/api/rest/sending-sms#example-1
