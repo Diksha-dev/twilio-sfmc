6143b5dfce3dcff5150de782071f232376365bcc
Diksha-dev <68223222+Diksha-dev@users.noreply.github.com>
2020-12-02 15:42:39 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -33,13 +33,18 @@ exports.messageresponse=function(req,res)
     console.log(req);
     console.log("messageresponseres"+res);
     console.log("body"+res.body);
-     res.on('data', function (chunk) {
-        data += chunk;
-    });
-     res.on('end', function () {
-        console.log(data);
-
-    });
+var url = require('url');
+var address = 'path';
+var q = url.parse(address, true);
+ 
+console.log(q.host); //returns 'localhost:8080'
+console.log(q.pathname); //returns '/index.php'
+console.log(q.search); //returns '?type=page&action=update&id=5221'
+ 
+var qdata = q.query; // returns an object: { type: page, action: 'update',id='5221' }
+ //returns 'page'
+console.log(qdata.Body); //returns 'update'
+console.log(qdata.To); //returns '5221
     
 };
 
