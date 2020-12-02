61324352913629d2826b841db1eea317774d66ac
Diksha-dev <68223222+Diksha-dev@users.noreply.github.com>
2020-12-02 12:30:00 +0530

    Update customActivity.js

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -100,7 +100,7 @@ define([
      //   var body = body1.concat(SentFrom);
         
        var phoneNumber = "{{Contact.Attribute.TwilioSecond.TwilioNumber}}" ;
-       var parsedNumber = `+91${phoneNumber}` 
+       var parsedNumber = `+${phoneNumber}` 
       //  console.log("Number"+parsedNumber);
         
         payload['arguments'].execute.inArguments = [{
