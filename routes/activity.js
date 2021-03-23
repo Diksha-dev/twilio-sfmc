846d03aa5f192101fb46c5bef356a6dcd28b6e9e
Diksha-dev <68223222+Diksha-dev@users.noreply.github.com>
2021-03-23 13:15:05 +0530

    Update activity.js

--- routes/activity.js
+++ routes/activity.js
@@ -85,7 +85,7 @@ exports.edit = function (req, res) {
 /*
  * POST Handler for /save/ route of Activity.
  */
-exports.save =  function async (req, res){
+exports.save =  function  (req, res){
     
     console.log("5 -- For Save");	
     console.log("4");	
